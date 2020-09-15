bdf9e2d8fdcf4b58a05ab5d61af12314bbbbb596
Kavin Raju S <rajukavin25@gmail.com>
2020-09-15 19:06:16 +0000

    Added commands to build, list, and run a docker container and expose port 80 to host's post 8000

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=boston_housing_prediction_container .
 
 # Step 2: 
 # List docker images
+docker images ls
 
 # Step 3: 
 # Run flask app
+docker run -it --name boston_housing_prediction_container -p 8000:80 boston_housing_prediction_container bash

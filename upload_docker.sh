dd138e7e7609eb66c626c3aa3f17e233f56c0775
Kavin Raju S <rajukavin25@gmail.com>
2020-09-15 19:09:16 +0000

    Added commands to upload the docker image to docker hub

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,16 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+# If you have not creates repo with the name "boston_housing_prediction_container", create one before running upload_docker.sh
+export dockerpath="kavinraju98/boston_housing_prediction_container"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username kavinraju98
+# docker tag source_image target_image
+docker tag boston_housing_prediction_container $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath

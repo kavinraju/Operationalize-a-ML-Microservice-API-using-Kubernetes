a51e8f98a80ecfed1fe8b59024ff66099fee6626
Kavin Raju S <rajukavin25@gmail.com>
2020-09-15 19:11:20 +0000

    Added commands to set up a kubernetes cluster by running it from the dockerhub, done in previoud commit, list the pods, and set port forward

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="kavinraju98/boston_housing_prediction_container:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+# Run boston_housing_prediction_container pod and expose to PORT 80
+kubectl run microservice --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+# Listen on port 8000 locally, forwarding to 80 in the pod (boston_housing_prediction_microservice)
+kubectl port-forward microservice 8000:80
